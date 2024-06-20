6738dd5c7d33093ba192cc65b3e3c1784aa87d33
jltdev15 <75570239+jltdev15@users.noreply.github.com>
2024-06-20 18:58:21 +0800

    first commit

---
+++ vue-watcheffect/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref, watch, watchEffect } from "vue"
+
+const todoId = ref(1)
+const data = ref(null)
+
+// watch(
+//   todoId,
+//   async () => {
+//     const response = await fetch(
+//       `https://jsonplaceholder.typicode.com/posts/${todoId.value}`
+//     )
+//     data.value = await response.json()
+//   },
+//   { immediate: true }
+// )
+
+watchEffect(async () => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${todoId.value}`
+  )
+  data.value = await response.json()
+})
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Watch and WatchEffect Functions Demo</h1>
+    <button @click="todoId++">Change ID</button>
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  padding: 24px;
+  font-size: 1.6rem;
+  text-align: center;
+}
+</style>
